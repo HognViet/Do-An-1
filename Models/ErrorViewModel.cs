b6c2b46d036a2198ebcbe4639973ba37efbd41b8
HognViet <ninhhongviet2@gmail.com>
2026-07-23 12:59:39 +0700

    prj rong dau tien

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace San_Pham_Do_An1.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
